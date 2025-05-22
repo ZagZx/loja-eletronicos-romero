4bd9a89420701416843fc4f1f565d419480332c4
ZagZx <azevedodvictor@gmail.com>
2025-05-22 09:20:10 -0300

    render template nas rotas

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+    pass
+
+@app.route("/produtos")
+def produtos():
+    return render_template('produtos.html')
+
+@app.route("/carrinho")
+def carrinho():
+    return render_template('carrinho.html')
+
