4a6e093fc9b0f61bf9f15fd79898c82a4b5f9e78
ZagZx <azevedodvictor@gmail.com>
2025-06-25 08:27:27 -0300

    url e engine mysql

--- database/db.py
+++ database/db.py
@@ -1,1 +1,13 @@
-from sql
+from sqlalchemy import create_engine, URL
+
+url_db = URL.create(
+    drivername='mysql',
+    username='root',
+    host='localhost',
+    port=3306
+    # database='eletronicos'
+)
+
+engine = create_engine(url= url_db)
+
+print(engine)
